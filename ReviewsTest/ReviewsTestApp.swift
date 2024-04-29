7a30a81deb0cfc63c365d2ab449106cf19d056dc
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-04-29 16:02:23 +0200

    first commit: Load initial files

---
+++ ReviewsTest/ReviewsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewsTestApp.swift
+//  ReviewsTest
+//
+//  Created by Marcin Jędrzejak on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReviewsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
